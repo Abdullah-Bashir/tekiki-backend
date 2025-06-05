f54539240523f84a9f98225ea94e4728526f6481
Abdullah-Bashir <killersaeed007@gmail.com>
2025-06-05 11:15:11 +0500

    i am commiting

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,6 @@ export const generateToken = (user) => {
     return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1d" })
 }
 
-
 export const authMiddleware = async (req, res, next) => {
     try {
         // Get token from Authorization header
